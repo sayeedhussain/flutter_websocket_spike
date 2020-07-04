6eb789690c115c81d0106744f68b907cf762756c
Sayeed <sayeedhussain19@gmail.com>
2020-07-04 16:15:15 +0530

    Update node websocket server and client

--- websocket-client.js
+++ websocket-client.js
@@ -5,13 +5,17 @@ const url = 'ws://localhost:8080'
 const connection = new WebSocket(url)
  
 connection.onopen = () => {
-  connection.send('Message From Client') 
+  console.log('Connection success') 
 }
  
 connection.onerror = (error) => {
-  console.log(`WebSocket error: ${error}`)
+  console.log('Connection failed: ', error)
+}
+
+connection.onclose = () => {
+  console.log('Connected closed') 
 }
  
 connection.onmessage = (e) => {
-  console.log(e.data)
+  console.log('Received message from server: ', e.data)
 }
--- websocket-server.js
+++ websocket-server.js
@@ -1,12 +1,64 @@
 // server.js
  
-const WebSocket = require('ws')
- 
+const WebSocket = require('ws') 
 const wss = new WebSocket.Server({ port: 8080 })
- 
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    console.log("Received message: " + message)
-    ws.send("Replaying message from server: " + message)
-  })
+const clients = {};
+
+setInterval(function() {  
+  for (var connectionId in clients) {
+    websocket = clients[connectionId];
+    websocket.send(getData());
+  }
+}, 5000);  
+
+const getUniqueID = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return s4() + s4() + '-' + s4();
+};
+
+wss.on('connection', websocket => {
+  var connectionId = getUniqueID();
+  clients[connectionId] = websocket
 })
+
+function getData() {
+  var date = new Date();
+  var data = 
+    {
+      "TimeStamp": date.toISOString(),
+      "Value": getCurrentValue(),
+      "Message": "mA",
+      "PinId": getPinId(),
+      "NSamples": 0,
+      "ChargeValue": getChargeValue(),
+      "ConcreteStatus": "High",
+      "Voltage": getVoltageValue()
+  }
+  return JSON.stringify(data);
+}
+
+function getPinId() {
+  return getRandomInt(0, 5);
+}
+
+function getCurrentValue() {
+  var currentValue = getRandomInt(32000, 33000);
+  var decimalValue = getRandomInt(1, 99) / 100;
+  return currentValue + decimalValue
+}
+
+function getVoltageValue() {
+  var currentValue = getRandomInt(32000, 33000);
+  var decimalValue = getRandomInt(1, 99) / 100;
+  return currentValue + decimalValue
+}
+
+function getChargeValue() {
+  return getRandomInt(19600000, 19610000);
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
